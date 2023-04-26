a07753a35688b3df0c86a32553799ff6ef1a2ab5
Prerna Ravi <prernaravi@dhcp-10-31-8-50.dyn.mit.edu>
2023-04-26 04:24:53 -0400

    added preliminary stats for before migration

--- main/src/components/Before.svelte
+++ main/src/components/Before.svelte
@@ -2,8 +2,46 @@
 </script>
   
 <div>
+    <h1 class="heading">Before Stats</h1>
+    <div>
+       <b>The following questions speak to intention to migrate vs actually taking steps to migrate: </b> 
+        <br/>
+        Ideally, if you had the opportunity, would you like to move permanently or for a long period to another country?
+        43.1% women said yes while the rest said no. 45.53% men said yes while rest said no.
+        <br/>
+        Are you planning to move to another country in the next 12 months?
+        13.37% women said yes. 19.52% men said yes. 
+        <br/>
+        Have you made any preparation to move to another country?
+        49% women said yes. 51.3% men said yes. 
+        <br/>
+        <b>Reasons for migration: </b><br/>
+        Search for a better job, salary or working conditions: 80.86% women and 88.25% men <br/>
+        Unemployment: 29.86% women and 32.71% men <br/>
+        Deterioration of livelihoods due to natural hazards (floods, droughts, volcanic eruptions, hurricanes, plagues, etc.): 3.91% women and 4.02% men <br/>
+        By the direct impact of a natural hazard: 1.92% women and 1.51% men <br/>
+        Due to loss of land due to processes of land use change: 0.32% women and 0.67% men <br/>
+        Lack of money to buy food: 17.59% women and 16.1% men <br/>
+        Lack of money to cover other basic needs (health, education, housing, clothing, services, etc.): 24.14% women and 25.16% men <br/>
+        To send remittances: 17.14% women and 19.79% men <br/>
+        For study: 2.89% women and 2.85% men <br/>
+        Domestic violence: 0.25% women and 0.16% men <br/>
+        Unsafety: 4.36% women and 5.2% men <br/>
+        Family reunification: 6.87% women and 4.36% men <br/>
+        For cultural reasons or custom: 1.79% women and 0.33% men <br/> 
+        For health (treatments, surgeries or medical consultations, medicines, etc.): 1.28% women and 1.17% men <br/>
+        Adventure Turism: 8.15% women and 5.03% men
+
+    </div>
 </div>
 
 <style>
+    @import url("https://fonts.googleapis.com/css?family=Ek+Mukta:400,300,500,700,800");
+    .heading {
+        margin: 0px;
+        padding: 20px;
+        text-align: center;
+        color: #c32093;
+    }
     
 </style>
