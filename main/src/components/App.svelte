4f6f04daaba1836716b62b388ef701cd3393dec9
LeliaPlusPlus <45381414+LeliaPlusPlus@users.noreply.github.com>
2023-04-29 13:09:21 -0400

    Update App.svelte

--- main/src/components/App.svelte
+++ main/src/components/App.svelte
@@ -4,7 +4,7 @@
     // import Scrolly from "./Scrolly.svelte";
     import Before from "./Before.svelte";
     import During from "./During.svelte";
-    import { animateDebt } from "./During.svelte";
+    import { animateDebt } from "./MigrationDebt.svelte";
     import After from "./After.svelte";
     import Scroller from "@sveltejs/svelte-scroller";
 
@@ -203,6 +203,31 @@
             <button on:click={() => collide()}>tick</button>
         </Scroller> -->
     </div>
+    <div class="DuringMigration">
+	<h1>During Migration</h1>
+	<div class="container">
+	    <div class="heading">
+	      <h2>How much debt do people incur to migrate?</h2>
+	    </div>
+	    <div class="counter-container">
+	      <div class="counter">
+		<!-- <img src="https://raw.githubusercontent.com/nemo0/animated-counter/29e12c0cb15e90c27faaef0d83fb2618126067db/icons/iconmonstr-time-19.svg" alt="timer" srcset="" class="icon"> -->
+		<img src="images/latinaicon.jpeg" alt="timer" srcset="" class="icon">
+		<h3 data-target="4425" class="count">0</h3>
+		<h6>Women's Migratory Debt</h6>
+	      </div>
+	      <div class="counter">
+		<!-- <img src="https://raw.githubusercontent.com/nemo0/animated-counter/29e12c0cb15e90c27faaef0d83fb2618126067db/icons/iconmonstr-coffee-11.svg" alt="Coffee" srcset="" class="icon"> -->
+		<img src="images/latinoicon.jpeg" alt="timer" srcset="" class="icon">
+		<h3 data-target="3575" class="count">0</h3>
+		<h6>Men's Migratory Debt</h6>
+	      </div>
+	    </div>
+	  </div>
+	  <div class = "debt-button">
+	      <button on:click={animateDebt}>Find out the cost of debt.</button>
+	  </div>
+     </div>
 
 </main>
 
@@ -477,6 +502,58 @@
         transform: rotateY(360deg);
     }
     }
+	
+h2 {
+    text-align: center;
+}
+
+.container {
+    text-align: center;
+}
+.debt-button{
+    text-align: center;
+}
+button {
+    background-color: #4a0a70; /* Purple */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+
+.container {
+    width: 80%;
+    margin: auto;
+  }
+  
+  .counter-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  
+  .counter {
+    text-align: center;
+  }
+  
+  .counter h3 {
+    padding: 0.5rem 0;
+    font-size: 2.5rem;
+    font-weight: 800;
+  }
+  
+  .counter h6 {
+    font-size: 2rem;
+    padding-bottom: 1rem;
+  }
+  
+  .icon {
+    height: 5rem;
+    width: auto;
+  }
 
     /* :root {
         --color-bg: #ffffff;
@@ -526,7 +603,8 @@
         color: #9e9e9e;
     }
 
-    h1 {
+    
+	{
         font-size: 72px;
         font-weight: 300;
         line-height: 2;
