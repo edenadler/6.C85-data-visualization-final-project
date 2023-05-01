87bb0781a2c9ec73838da7354032a9b17625b473
Eden Adler <edenadler@C02G915GMD6P.tld>
2023-04-30 22:31:19 -0400

    cleanup

--- main/src/routes/+page.svelte
+++ main/src/routes/+page.svelte
@@ -5,15 +5,6 @@
 <App />
 
 <style>
-    /* main {
-        text-align: center;
-        font-family: "Nunito", sans-serif;
-        font-weight: 300;
-        line-height: 2;
-        font-size: 24px;
-        color: var(--color-text);
-        margin-top: 100px;
-    } */
 
     body {
         margin: 0px;
