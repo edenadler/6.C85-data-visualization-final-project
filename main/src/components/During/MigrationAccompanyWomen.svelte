45f9f6ea5d2b3eb5ab9a7531fa8116b65670020d
LeliaPlusPlus <45381414+LeliaPlusPlus@users.noreply.github.com>
2023-05-11 23:03:21 -0400

    Update MigrationAccompanyWomen.svelte

--- main/src/components/During/MigrationAccompanyWomen.svelte
+++ main/src/components/During/MigrationAccompanyWomen.svelte
@@ -4,13 +4,13 @@
 
     let svg;
     let g;
-    let height = 250;
+    let height = 500;
     let radius = height/2;
     // arc path and angle generator
     let arcGenerator = d3.arc()
       .innerRadius(radius * 0.5)         // This is the size of the donut hole
       .outerRadius(radius * 0.8)
-      .padAngle(0)
+      .padAngle(0.02)
       .cornerRadius(0);
     let data = [{name: "Family", value: 0.437}, {name: "Coyote", value: 0.275}, {name: "Alone", value: 0.215}, {name: "Other", value: 0.073}];
 
@@ -34,138 +34,23 @@
       let svg_one = d3.select("svg g");
       console.log('here', svg_one.selectAll("path"))
       
-
-      // d3.select("svg g")
-      //   .selectAll("path")
-      //   .data(arc_data)
-      //   .append('text')
-      //   .text(function(d){ 
-      //     console.log('d', d.data.name)
-      //     return "grp " + d.data.name})
-      //   .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
-      //   .style("text-anchor", "middle")
-      //   .style("font-size", 17)
     })
-
-
-
-    // onMount(() => {
-    //   createChart();
-    // })
-    
-    // let createChart = function() {
-    //       // set the dimensions and margins of the graph
-    //     let pie_width = 450;
-    //     let pie_height = 450;
-    //     let pie_margin = 40;
-
-    //     // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-    //     let radius = Math.min(pie_width, pie_height) / 2 - pie_margin;
-    //     // append the svg object to the div called 'debt-source-women'
-    //     let svg = d3.select("div")
-    //         .append("svg")
-    //             .attr("width", pie_width)
-    //             .attr("height", pie_height)
-    //         .append("g")
-    //             .attr("transform", "translate(" + pie_width / 2 + "," + pie_height / 2 + ")");  
-        
-    //     let data = {"Family": 0.437, "Coyote": 0.275, "Alone": 0.215, "Other": 0.073}
-    //     // var data = [{"label": "Loans from Creditors", "percentage": 0.76}, 
-    //     //               {"label": "Loans from Family/Friends", "percentage": 0.24}]
-    //     // set the color scale
-    //     let color = d3.scaleOrdinal()
-    //         .domain([0.073, 0.215, 0.275, 0.437])
-    //         .range(["#4a0a70", "#fdbb58", "#ce2093", "#fe872f"])
-    //     // Compute the position of each group on the pie:
-    //     let pie = d3.pie()
-    //         .value(function(d) {return d.value; })
-    //     let data_ready = pie(Object.entries(data))
-
-    //     let tooltip = d3.select(".tooltip-container").append("div")
-    //     //var tooltip = d3.select("#accompany-women")
-    //       .attr("class", "tooltip")
-    //       .style("opacity", 0.5);
-            
-    //     tooltip.append("rect")
-    //       .attr("width", 30)
-    //       .attr("height", 20)
-    //       .attr("fill", "#ffffff")
-    //       .style("opacity", 0.5);
-        
-    //     tooltip.append("div")
-    //       .attr("x", 15)
-    //       .attr("dy", "1.2em")
-    //       .style("text-anchor", "middle")
-    //       .attr("font-size", "1.5em")
-    //       .attr("font-weight", "bold");
-    //     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    //     svg
-    //         .selectAll('whatever')
-    //         .data(data_ready)
-    //         .enter()
-    //         .append('path')
-    //         .attr('d', d3.arc()
-    //         .innerRadius(100)         // This is the size of the donut hole
-    //         .outerRadius(radius)
-    //         )
-    //         .attr('fill', function(d){ return(color(d.data.key)) })
-    //         .attr("stroke", "black")
-    //         .style("stroke-width", "2px")
-    //         //.style("opacity", 0.7)
-    //         .on("mouseover", function() {
-    //             tooltip.style("display", null);
-    //         })
-    //         .on("mousemove", function(d) {
-    //             tooltip.transition().duration(200)
-    //               .style("opacity", 0.9);
-    //             tooltip.select("div").html(d.data.key + " <br><strong>" + d.data.value + "</strong>")
-    //             //tooltip.select("#accompany-women").html(d.data.key + " <br><strong>" + d.data.value + "</strong>")
-    //               .style("position", "fixed")
-    //               .style("text-align", "center")
-    //               .style("width", "120px")
-    //               .style("height", "45px")
-    //               .style("padding", "2px")
-    //               .style("font", "12px sans-serif")
-    //               //.style("background", "lightsteelblue")
-    //               .style("background", "pink")
-    //               .style("border", "0px")
-    //               .style("border-radius", "8px")
-    //               //.style("left", (d3.event.pageX + 15) + "px")
-    //               .style("left", (d3.event.pageX) + "px")
-    //               //.style("top", (d3.event.pageY - 28) + "px");
-    //               .style("top", (d3.event.pageY - 100) + "px");
-    //             d3.select(this.firstChild).transition()
-    //               .attr("d", arcOver);
-            
-    //         })
-    //         .on("mouseout", function() {
-    //             tooltip.style("display", "none")
-    //             d3.select(this.firstChild).transition()
-    //               .attr("d", arc)
-    //               .attr("stroke", "none");
-    //         })
-        
-    //     svg.append("g").append("text")
-    //       //.attr("x", pie_width/6)
-    //       .attr("y", -205)
-    //       .attr("text-anchor", "middle")
-    //       .style("font-size", "16px")
-    //       .text("Who accompanies women on the migration journey?");
-    // }
+// fill={index === hovered ? "pink": arc_color(d.data["value"])}
 </script>
 
 <div class="visualization">
-	<svg width="500" height={height} bind:this={svg}>
-		<g transform="translate(250, 120)" bind:this={g}>
+	<svg width="600" height="700" bind:this={svg}>
+		<g transform="translate(270, 300)" bind:this={g}>
 			{#each arc_data as d, index}
       {console.log(d)}
+	  <!-- svelte-ignore a11y-no-onchange -->
 			<path 
 				d={arcGenerator({
 					startAngle: d.startAngle,
 					endAngle: d.endAngle
 				})}
 				fill={arc_color(d.data["value"])}
-        fill-opacity={index === hovered ? 0.8: 1}
+        fill-opacity={index === hovered ? 0.3: 1}
         text={d.data.name}
 				on:mouseover={
 				(event) => { 
@@ -177,16 +62,34 @@
 				}}
 				on:mouseout={(event) => { hovered = -1; }}
 			/>
-      <text text-anchor="middle" alignment-baseline="middle" transform="translate({label.centroid(d)})">{d.data.name}</text>
+			<text x="-265" y="-160">Alone: 21.5%</text>
+			<text x="125" y="-160">Family: 43.7%</text>
+			<text x="-235" y="215">Coyote: 27.5%</text>
+			<text x="-190" y="-220">Other: 7.3%</text> 
+			<polyline points="180,-80 220,-130 220, -150" style="fill:#fff295;stroke:black;stroke-width:2" />
+			<polyline points="-30,-195 -37,-225, -55,-225" style="fill:#fff295;stroke:black;stroke-width:2" />
+			<polyline points="-175,-95 -215,-125 -215,-150" style="fill:#fff295;stroke:black;stroke-width:2" />
+			<polyline points="-150,130 -185,165 -185, 190" style="fill:#fff295;stroke:black;stroke-width:2" />
 			{/each}
 		</g>
 	</svg>
 	<div
 		class={hovered === -1 ? "tooltip-hidden": "tooltip-visible"}	
-		style="left: {recorded_mouse_position.x + 40}px; top: {recorded_mouse_position.y + 40}px"
+		style="left: {160}px; top: {240}px"
 	>
   {#if hovered !== -1}
-  {arc_data[hovered].name}
+  {#if hovered === 0}
+			82,593 women travel with their family each year
+			{/if}
+			{#if hovered === 1}
+			51,975 women travel with a coyote each year
+			{/if}
+			{#if hovered === 2}
+			40,635 women travel alone each year
+			{/if}
+			{#if hovered === 3}
+			13,797 women travel with other company each year
+			{/if}
   {/if}
 	</div>
 </div>
@@ -194,7 +97,7 @@
 <style>
 
   .visualization {
-		font: 25px sans-serif;
+		font: 23px sans-serif;
 		margin-top: 1px;
 		text-align: middle;
     position: relative;
@@ -217,7 +120,7 @@
 		font: 25px sans-serif;
 		font-family: "Nunito", sans-serif;
 		visibility: visible;
-		background-color: #f0dba8;
+		/* background-color: #f0dba8; */
 		border-radius: 10px;
 		width: 200px;
 		color: black;
